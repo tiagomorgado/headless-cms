99ac6bfd6475b14fdc4805a0ab0834fd4919dc40
Tiago Morgado <Tiago.alcm@icloud.com>
2023-06-22 22:21:56 +0200

    token update

--- src/fetch.jsx
+++ src/fetch.jsx
@@ -3,7 +3,14 @@ import { createClient } from 'contentful'
 const client = createClient({
     space: 'q4xa2015toko',
     environment: 'master',
-    accessToken: '1n1PiKp7Ygn1sK4yGZDfgfJb0LtBsTar3VS5Ue3p5Fo',
+    /* accessToken: '1n1PiKp7Ygn1sK4yGZDfgfJb0LtBsTar3VS5Ue3p5Fo', */
 });
+
+const useFetchProjects = () => {
+    const []
+}
+
+
+
 
 client.getEntries({content_type: 'projects'}).then((response) => console.log(response))
