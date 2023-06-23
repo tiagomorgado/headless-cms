4bdf953cb774ed9de2d49e72d5d93958ca43328e
Tiago Morgado <Tiago.alcm@icloud.com>
2023-06-23 22:27:45 +0200

    final changes

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,11 +1,36 @@
 import { useFetchProjects } from '../fetch'
 
 const Projects = () => {
+   const {isLoading, projects} = useFetchProjects()
 
-   const myData = useFetchProjects()
-   console.log('myData', myData);
+   if(isLoading) {
+    return (
+        <section className='projects'>
+            <h2>Loading...</h2>
+        </section>
+    )
+   }
+
   return (
-    <h2>Projects</h2>
+    <section className='projects'>
+        <div className="title">
+        <h2>Projects</h2>
+        <div className="title-underline"></div>
+        </div>
+        <div className="projects-center">
+            {
+                projects.map((project) => {
+                    const {id, img, url, title} = project
+                    return (
+                        <a key={id} href={url} target='_blank' rel='noreferrer' className='project'>
+                            <img src={img} alt={title} className='img'/>
+                            <h5>{title}</h5>
+                        </a>
+                    )
+                })
+            }
+        </div>
+        </section>
   )
 }
 export default Projects
